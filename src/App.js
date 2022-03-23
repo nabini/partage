d0e3610b28ec303852aa09a4f73b1ac5b9d97b83
Kone <kone@gmail.com>
2022-03-23 23:15:13 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Home from './pages/Home';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Home  />
+      {/* https://nabini.github.io/partage */}
+    </div>
+  );
+}
+
+export default App;
